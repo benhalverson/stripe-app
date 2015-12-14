4bb7cfc146515de36d138706c2e6b71e8c0ad333
Ben Halverson <benhalverson33@gmail.com>
2015-12-13 21:22:02 -0800

    added books and users routes

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+var User = require('../models/user');
+
+// users routes
+
+router.post('/register', function(req, res){
+  console.log('req.body', req.body);
+  User.register(req.body, function(err, user){
+    res.status(err || !user ? 400 : 200).send(err || {token: user.token()});
+  });
+});
+
+router.post('/login', function(req, res){
+  User.authenticate(req.body, function(err, user){
+    if(err || !user) {
+      res.status(400).send(err);
+    } else {
+      var token = user.token();
+      res.send({token: token });
+    }
+  });
+});
+
+module.exports = router;
