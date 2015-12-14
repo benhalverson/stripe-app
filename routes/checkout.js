71bc515feb5a6639fb6c6784e9d257ee515421ed
Ben Halverson <benhalverson33@gmail.com>
2015-12-14 03:27:11 -0800

    it works

--- routes/checkout.js
+++ routes/checkout.js
@@ -3,14 +3,14 @@
 var express = require('express');
 var router = express.Router();
 
-var stripe = require("stripe")("sk_test_NdLgBtk5yLLjWKyNLrbP0XRd");
+var stripe = require("stripe")("sk_test_5PEJEgn4aQavdoBAwiugPHsz");
 
 router.post('/', function(req, res) {
   var tokenObj = req.body.tokenObj;
   var book = req.body.book;
 
   var charge = stripe.charges.create({
-    amount: Math.round(book.price * 100),
+    amount: 14.00 * 100,
     currency: "usd",
     source: tokenObj.id,
     description: `${book.title} by ${book.author}`
