cef6a0797ce5b1bca662ae91f2f1b23d2af4f082
Ben Halverson <benhalverson33@gmail.com>
2015-12-13 21:02:52 -0800

    setup app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var PORT = process.env.POR || 3000;
+var MONGO_URL = process.env.MONGO_URL || 'mongo://localhost:27017/payments';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var morgan = require('morgan');
+
+var mongoose = require('mongoose');
+mongoose.connect('MONGO_URL', function(err){
+  if(err) return console.log(err);
+  console.log('MongoDB connected to: ', MONGO_URL);
+});
+
+var app = express();
+
+app.set('view engine', 'jade');
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static('public'));
+
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+app.use('/books', require('./routes/books'));
+app.use('/checkout', require('./routes/checkout'));
+
+app.use(function(req, res){
+  res.status(404).render('404');
+});
+
+app.listen(PORT, function(){
+  console.log('Listening on port ', PORT);
+});
